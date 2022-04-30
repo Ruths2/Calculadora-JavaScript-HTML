425e57098ed788f811c455f93a90c6a66574c36f
Ruths2 <ruth09@yahoo.com>
2022-04-30 19:44:57 -0300

    Cria as function: subtrair, dividir e multiplicar

--- calculator.js
+++ calculator.js
@@ -1,32 +1,39 @@
 function calculadora() {
-    const operacao = Number(prompt('Escolha uma operacao:\n 1- Somar\n 2- Subtrair\n 3- Dividir\n 4- Multiplicar'));
+  const operacao = Number(prompt('Escolha uma operacao:\n 1- Somar\n 2- Subtrair\n 3- Dividir\n 4- Multiplicar'));
 
-    let n1 = Number(prompt('Insira o primeiro valor:'));
-    let n2 = Number(prompt('Insira o segundo valor:'));
-    let resultado;
+  let n1 = Number(prompt('Insira o primeiro valor:'));
+  let n2 = Number(prompt('Insira o segundo valor:'));
+  let resultado;
 
-    function somar() {
-        resultado = n1 + n2;
-        alert(n1 + ' + ' + n2 + ' = ' + resultado);
-    }
+  function somar() {
+    resultado = n1 + n2;
+    alert(n1 + ' + ' + n2 + ' = ' + resultado);
+  }
+  
+  function subtrair(){
+    resultado = n1 - n2;
+    alert(n1 + ' - ' + n2 + ' = ' + resultado);
+  }
+  
+  function dividir() {
+    resultado = n1 / n2;
+    alert(n1 + ' / ' + n2 + ' = ' + resultado);
+  }
+  
+  function multiplicar(){
+    resultado = n1 * n2;
+    alert(n1 + ' * ' + n2 + ' = ' + resultado);
+  }
 
-    if (operacao == 1)
-    {
-        somar();
-    }
-    else if (operacao == 2)
-    {
-        subtrair();
-    }
-    else if (operacao == 3)
-    {
-        dividir();
-    }
-    else if (operacao == 4)
-    {
-        multiplicar();
-    }
+  if (operacao == 1) {
+    somar();
+  }else if (operacao == 2) {
+    subtrair();
+  }else if (operacao == 3) {
+    dividir();
+  }else if (operacao == 4) {
+    multiplicar();
+  }
 }
-
 calculadora();
 
