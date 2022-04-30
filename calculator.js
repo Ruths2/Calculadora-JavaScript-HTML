a19bc0031cfc39b27e9360b1af6755c82773d039
Ruths2 <ruth09@yahoo.com>
2022-04-30 20:33:22 -0300

    Cria function novaOperação

--- calculator.js
+++ calculator.js
@@ -1,5 +1,5 @@
 function calculadora() {
-  const operacao = Number(prompt('Escolha uma operacao:\n 1- Somar\n 2- Subtrair\n 3- Dividir\n 4- Multiplicar'));
+  const operacao = Number(prompt('Escolha uma operação:\n 1- Somar\n 2- Subtrair\n 3- Dividir\n 4- Multiplicar'));
 
   let n1 = Number(prompt('Insira o primeiro valor:'));
   let n2 = Number(prompt('Insira o segundo valor:'));
@@ -8,21 +8,38 @@ function calculadora() {
   function somar() {
     resultado = n1 + n2;
     alert(n1 + ' + ' + n2 + ' = ' + resultado);
+    novaOperacao();
   }
   
   function subtrair(){
     resultado = n1 - n2;
     alert(n1 + ' - ' + n2 + ' = ' + resultado);
+    novaOperacao();
   }
   
   function dividir() {
     resultado = n1 / n2;
     alert(n1 + ' / ' + n2 + ' = ' + resultado);
+    novaOperacao();
   }
   
   function multiplicar(){
     resultado = n1 * n2;
     alert(n1 + ' * ' + n2 + ' = ' + resultado);
+    novaOperacao();
+  }
+  
+  function novaOperacao(){
+    let opcao = prompt('Deseja fazer uma nova operação?\n 1- Sim\n 2- Não');
+    
+    if(opcao == 1) {
+      calculadora();
+    }else if(opcao == 2){
+      alert('Muito obrigado ' + String.fromCodePoint(0x1F917) + '\nVolte sempre ' + String.fromCodePoint(0x2763));
+    }else{
+      alert('Opcao invalida');
+      novaOperacao();
+    }
   }
 
   if (operacao == 1) {
