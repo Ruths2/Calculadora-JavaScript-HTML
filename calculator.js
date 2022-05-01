347134d682785239973290208c3b6d316f65cc5c
Ruths2 <ruth09@yahoo.com>
2022-05-01 00:54:45 -0300

    Cria o um if/else para solucionar o problema de entrada inválida nos valores 1 e 2 e coloca o switch/case no lugar do if/else para a chamada dos metodos na operacao.

--- calculator.js
+++ calculator.js
@@ -6,54 +6,80 @@ function calculadora() {
     calculadora();
   }else {
     let n1 = Number(prompt('Insira o primeiro valor:'));
-    let n2 = Number(prompt('Insira o segundo valor:'));
-    let resultado;
     
-    function somar() {
-      resultado = n1 + n2;
-      alert(n1 + ' + ' + n2 + ' = ' + resultado);
-      novaOperacao();
-    }
-
-    function subtrair() {
-      resultado = n1 - n2;
-      alert(n1 + ' - ' + n2 + ' = ' + resultado);
-      novaOperacao();
-    }
-
-    function dividir() {
-      resultado = n1 / n2;
-      alert(n1 + ' / ' + n2 + ' = ' + resultado);
-      novaOperacao();
-    }
-
-    function multiplicar() {
-      resultado = n1 * n2;
-      alert(n1 + ' * ' + n2 + ' = ' + resultado);
-      novaOperacao();
-    }
-
-    function novaOperacao() {
-      let opcao = prompt('Deseja fazer uma nova operação?\n 1- Sim\n 2- Não');
-
-      if (opcao == 1) {
+    if(!n1){
+      alert(String.fromCodePoint(0x2716) + ' Opção inválida - Digite uma opção válida');
+      calculadora();
+    }else{
+      let n2 = Number(prompt('Insira o segundo valor:'));
+      let resultado;
+      
+      if (!n2){
+        alert(String.fromCodePoint(0x2716) + ' Opção inválida - Digite uma opção válida');
         calculadora();
-      }else if (opcao == 2) {
-        alert('Muito obrigado ' + String.fromCodePoint(0x1F917) + '\nVolte sempre ' + String.fromCodePoint(0x2763));
-      }else {
-        alert('Opcao invalida');
-        novaOperacao();
-      }
-    }
+      }else{
+        function somar() {
+          resultado = n1 + n2;
+          alert(n1 + ' + ' + n2 + ' = ' + resultado);
+          novaOperacao();
+        }
 
-    if (operacao == 1) {
-      somar();
-    }else if (operacao == 2) {
-      subtrair();
-    }else if (operacao == 3) {
+        function subtrair() {
+          resultado = n1 - n2;
+          alert(n1 + ' - ' + n2 + ' = ' + resultado);
+          novaOperacao();
+        }
+
+        function dividir() {
+          resultado = n1 / n2;
+          alert(n1 + ' / ' + n2 + ' = ' + resultado);
+          novaOperacao();
+        }
+
+        function multiplicar() {
+          resultado = n1 * n2;
+          alert(n1 + ' * ' + n2 + ' = ' + resultado);
+          novaOperacao();
+        }
+      }
+
+      function novaOperacao() {
+        let opcao = prompt('Deseja fazer uma nova operação?\n 1- Sim\n 2- Não');
+
+        if (opcao == 1) {
+          calculadora();
+        }else if (opcao == 2) {
+          alert('Muito obrigado ' + String.fromCodePoint(0x1F917) + '\nVolte sempre ' + String.fromCodePoint(0x2763));
+        }else {
+          alert('Opcao invalida');
+          novaOperacao();
+        }
+      }
+
+      /*if (operacao == 1) {
+        somar();
+      }else if (operacao == 2) {
+        subtrair();
+      }else if (operacao == 3) {
       dividir();
-    }else if (operacao == 4) {
-      multiplicar();
+      }else if (operacao == 4) {
+        multiplicar();
+      }*/
+      
+      switch(operacao){
+        case 1: 
+          somar();
+          break;
+        case 2: 
+          subtrair();
+          break;
+        case 3: 
+          dividir();
+          break;
+        case 4: 
+          multiplicar();
+          break;
+      }
     }
   }
 }
